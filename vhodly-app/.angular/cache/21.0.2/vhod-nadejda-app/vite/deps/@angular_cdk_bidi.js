29c261485817ac96d4e6318f2e7cedfe062f4dbc
Krizerion <kris.petrov.93@gmail.com>
2026-01-28 20:35:16 +0200

    test

---
+++ vhodly-app/.angular/cache/21.0.2/vhod-nadejda-app/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,15 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-L7SLR225.js";
+import "./chunk-MRU2T2FA.js";
+import "./chunk-RSS3ODKE.js";
+import "./chunk-GOMI4DH3.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
